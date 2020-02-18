7003b049002fc649e0364ef3bb49107f6d82f7b9
Maxim Muzafarov <maxmuzaf@gmail.com>
2020-02-18 21:35:07 +0300

    GG-29114 [IGNITE-12472] upgrade checkstyle version up to 8.21 (#7174)

--- modules/core/src/main/java/org/apache/ignite/spi/discovery/tcp/ServerImpl.java
+++ modules/core/src/main/java/org/apache/ignite/spi/discovery/tcp/ServerImpl.java
@@ -8388,7 +8388,6 @@ class ServerImpl extends TcpDiscoveryImpl {
             }
         }
 
-
         /**
          * @param laps Number of discovery ring laps passed by the message.
          * @param msg Message taken from the queue.
